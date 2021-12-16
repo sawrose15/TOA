bfe3290d00ca6c9fd3a2902e10e788319c107dde
Saroj Maharjan <saroj.maharjan1993@gmail.com>
2021-12-16 12:56:50 +0800

    Added Ktlint, git-hooks, github Action and Danger

--- app/src/test/java/com/sawrose/toa/ExampleUnitTest.kt
+++ app/src/test/java/com/sawrose/toa/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.sawrose.toa
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
