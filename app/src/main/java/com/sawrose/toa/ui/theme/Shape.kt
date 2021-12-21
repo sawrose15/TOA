55fe44ea92795adb970dfd2c0198acba10d0ffa3
Saroj Maharjan <saroj.maharjan1993@gmail.com>
2021-12-21 08:46:00 +0800

    Adding Buttons

--- app/src/main/java/com/sawrose/toa/ui/theme/Shape.kt
+++ app/src/main/java/com/sawrose/toa/ui/theme/Shape.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
+val ButtonShape = RoundedCornerShape(50)
+
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
