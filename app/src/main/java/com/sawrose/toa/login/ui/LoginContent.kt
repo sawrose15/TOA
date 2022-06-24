cce703a3cee74f7bba4b44147a5c1ff3959d8896
Saroj Maharjan <saroj.maharjan1993@gmail.com>
2022-06-24 22:02:28 +0800

    changes on login

--- app/src/main/java/com/sawrose/toa/login/ui/LoginContent.kt
+++ app/src/main/java/com/sawrose/toa/login/ui/LoginContent.kt
@@ -2,7 +2,14 @@ package com.sawrose.toa.login.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
